e84f135fca2fa3ab264fa80e353462af2b3fe0fa
@diegote22 <diegote_21_82@hotmail.com>
2024-06-03 09:37:18 -0300

    Finalización de WebDev.innovando

---
+++ Js.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("header");
+  
+    window.addEventListener("scroll", function() {
+      if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  });
+  
